f49741a592eb14fce983506d6cbf3a9c0871d586
JlKol1 <yulyak2603@gmail.com>
2021-03-01 19:46:10 +0300

    task basic js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+    
+if ( Array.isArray(members) == false){
+    return false
+} else if (Array.isArray(members) == true){
+    let arr = [];
+for(let i=0;i<members.length;i++){
+        if (typeof members[i]=='string'){
+            arr.push(members[i])
+        }
+}
+    return arr.join(',').replace(/ /g,"").split(',').map(el => el.slice(0,1)).sort().join('').toUpperCase().match(/[A-Z]/gi).join('')
+}
+}
