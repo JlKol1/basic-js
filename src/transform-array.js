ba5a2753f86bc0a0ab09abe6cf11144fa6643f0d
JlKol1 <yulyak2603@gmail.com>
2021-03-04 14:36:00 +0300

    init: add basic-js-tasK

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    for (let i=0;i<arr.length;i++){
+         if (arr == []){
+            return []
+        }else if(arr[i] === '--double-next'){
+                arr[i] = arr[i+1] 
+            }else if(arr[i] === '--double-prev'){
+                arr[i] = arr[i-1] 
+            }else if(arr[i] === '--discard-next'){
+                delete arr[i+1] 
+               }else if(arr[i] === '--discard-prev'){
+                   delete arr[i-1] 
+               }
+        }
+        return arr.filter(Number)
 };
